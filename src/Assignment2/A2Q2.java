c8a27ef95d5c6247b5845902e9756e7a181752f9
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-02-16 10:56:41 -0500

    A2Q2 completed.

--- src/Assignment2/A2Q2.java
+++ src/Assignment2/A2Q2.java
@@ -6,6 +6,7 @@ package Assignment2;
 
 import becker.robots.City;
 import becker.robots.Direction;
+import becker.robots.IPredicate;
 import becker.robots.RobotSE;
 import becker.robots.Thing;
 import becker.robots.Wall;
@@ -21,47 +22,48 @@ public class A2Q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      //Create a city for the Robot      
-  City townsburg = new City();
-  
-  //Put a Robot in Townsburg
-  RobotSE andy = new RobotSE(townsburg, 4, 1, Direction.EAST);
-  
-  //Set the color of the Robot
-  andy.setColor(Color.CYAN);
-  
-  andy.setLabel("A");
-  
-  for(int i = 1; i <= 8; i = i + 1){
-      new Wall (townsburg, 4,i, Direction.SOUTH);
-  }
-  new Wall (townsburg, 4, 2, Direction.EAST);
-  new Wall (townsburg, 4, 4, Direction.EAST);
-  new Wall (townsburg, 4, 5, Direction.EAST);
-  new Wall (townsburg, 4, 7, Direction.EAST);
-  
-  new Thing (townsburg, 4, 8);
-  
-  andy.move();
-  
-  while(andy.frontIsClear()){
-  andy.move();
+        //Create a city for the Robot      
+        City townsburg = new City();
 
-    } 
-  while(!andy.frontIsClear()){
-  andy.turnLeft();
-  andy.move();
-  andy.turnRight();
-  andy.move();
-  andy.turnRight();
-  andy.move();
-  andy.turnLeft();
-  
-  if(!andy.frontIsClear()){
- 
-  }else{
-  andy.move();
-    }
-}
+        //Put a Robot in Townsburg
+        RobotSE andy = new RobotSE(townsburg, 4, 1, Direction.EAST);
+
+        //Set the color of the Robot
+        andy.setColor(Color.CYAN);
+
+        andy.setLabel("A");
+
+        //create a line of walls
+        for (int i = 1; i <= 8; i = i + 1) {
+            new Wall(townsburg, 4, i, Direction.SOUTH);
+        }
+        //create the rest of the walls
+        new Wall(townsburg, 4, 2, Direction.EAST);
+        new Wall(townsburg, 4, 4, Direction.EAST);
+        new Wall(townsburg, 4, 5, Direction.EAST);
+        new Wall(townsburg, 4, 6, Direction.EAST);
+
+        //create the finish line
+        new Thing(townsburg, 4, 8);
+
+        //while andy cannot pick a thing he will follow the if statements
+        while (!andy.canPickThing()) {
+
+        //while the front is not clear andy will follow the provided instructions
+        if (!andy.frontIsClear()) {
+        andy.turnLeft();
+        andy.move();
+        andy.turnRight();
+        andy.move();
+        andy.turnRight();
+        andy.move();
+        andy.turnLeft();
+        } else {
+        //if there is no wall andy will move
+        andy.move();
+        }
+
+        }
+
     }
 }
