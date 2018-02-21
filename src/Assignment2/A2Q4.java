7ffe1ebd205002d8422f4d50962b248be066e1f9
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-02-21 11:18:40 -0500

    A2Q4 just started.

---
+++ src/Assignment2/A2Q4.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment2;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.IPredicate;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+import java.awt.Color;
+
+/**
+ *
+ * @author watsa9604
+ */
+public class A2Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        //Create a city for the Robot      
+        City townsburg = new City();
+
+        //Put a Robot in Townsburg
+        RobotSE andy = new RobotSE(townsburg, 2, 1, Direction.WEST);
+
+        //Set the color of the Robot
+        andy.setColor(Color.CYAN);
+
+        andy.setLabel("A");
+        
+        new Wall (townsburg, 1,1, Direction.NORTH);
+        new Wall (townsburg, 1,1, Direction.SOUTH);
+        new Wall (townsburg, 1,1, Direction.EAST);
+        new Wall (townsburg, 1,1, Direction.WEST);
+        new Wall (townsburg, 4,1, Direction.NORTH);
+        new Wall (townsburg, 4,1, Direction.SOUTH);
+        new Wall (townsburg, 4,1, Direction.EAST);
+        new Wall (townsburg, 4,1, Direction.WEST);
+        new Wall (townsburg, 1,4, Direction.NORTH);
+        new Wall (townsburg, 1,4, Direction.SOUTH);
+        new Wall (townsburg, 1,4, Direction.EAST);
+        new Wall (townsburg, 1,4, Direction.WEST);
+        new Wall (townsburg, 4,4, Direction.NORTH);
+        new Wall (townsburg, 4,4, Direction.SOUTH);
+        new Wall (townsburg, 4,4, Direction.EAST);
+        new Wall (townsburg, 4,4, Direction.WEST);
+        new Wall (townsburg, 2,2, Direction.NORTH);
+        new Wall (townsburg, 2,2, Direction.WEST);
+        new Wall (townsburg, 3,2, Direction.SOUTH);
+        new Wall (townsburg, 3,2, Direction.WEST);
+        new Wall (townsburg, 3,3, Direction.SOUTH);
+        new Wall (townsburg, 3,3, Direction.EAST);
+        new Wall (townsburg, 2,3, Direction.NORTH);
+        new Wall (townsburg, 2,3, Direction.EAST);
+        
+        
+        
+        
+    }
+}
