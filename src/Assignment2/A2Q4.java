d07e43274558884c2bee1d596dd5f089e13936da
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-02-23 10:46:28 -0500

    A1Q4 1 Complete.

--- src/Assignment2/A2Q4.java
+++ src/Assignment2/A2Q4.java
@@ -21,68 +21,70 @@ public class A2Q4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+
         //Create a city for the Robot      
         City townsburg = new City();
 
         //Put a Robot in Townsburg
-        RobotSE andy = new RobotSE(townsburg, 2, 1, Direction.WEST);
+        RobotSE andy = new RobotSE(townsburg, 3, 1, Direction.NORTH);
 
         //Set the color of the Robot
         andy.setColor(Color.CYAN);
 
         andy.setLabel("A");
-        
-        new Wall (townsburg, 1,1, Direction.NORTH);
-        new Wall (townsburg, 1,1, Direction.SOUTH);
-        new Wall (townsburg, 1,1, Direction.EAST);
-        new Wall (townsburg, 1,1, Direction.WEST);
-        new Wall (townsburg, 4,1, Direction.NORTH);
-        new Wall (townsburg, 4,1, Direction.SOUTH);
-        new Wall (townsburg, 4,1, Direction.EAST);
-        new Wall (townsburg, 4,1, Direction.WEST);
-        new Wall (townsburg, 1,4, Direction.NORTH);
-        new Wall (townsburg, 1,4, Direction.SOUTH);
-        new Wall (townsburg, 1,4, Direction.EAST);
-        new Wall (townsburg, 1,4, Direction.WEST);
-        new Wall (townsburg, 4,4, Direction.NORTH);
-        new Wall (townsburg, 4,4, Direction.SOUTH);
-        new Wall (townsburg, 4,4, Direction.EAST);
-        new Wall (townsburg, 4,4, Direction.WEST);
-        new Wall (townsburg, 2,2, Direction.NORTH);
-        new Wall (townsburg, 2,2, Direction.WEST);
-        new Wall (townsburg, 3,2, Direction.SOUTH);
-        new Wall (townsburg, 3,2, Direction.WEST);
-        new Wall (townsburg, 3,3, Direction.SOUTH);
-        new Wall (townsburg, 3,3, Direction.EAST);
-        new Wall (townsburg, 2,3, Direction.NORTH);
-        new Wall (townsburg, 2,3, Direction.EAST);
-        
-     while(andy.getStreet()==2)  { 
-       andy.move();
-       andy.turnRight();
-       andy.move(2);
-       andy.turnRight();
-       andy.move(2);
-       andy.turnRight();
-       andy.move();
-       andy.turnLeft();
 
-}
+        //create king Java's castle
+        new Wall(townsburg, 1, 1, Direction.NORTH);
+        new Wall(townsburg, 1, 1, Direction.SOUTH);
+        new Wall(townsburg, 1, 1, Direction.EAST);
+        new Wall(townsburg, 1, 1, Direction.WEST);
+        new Wall(townsburg, 4, 1, Direction.NORTH);
+        new Wall(townsburg, 4, 1, Direction.SOUTH);
+        new Wall(townsburg, 4, 1, Direction.EAST);
+        new Wall(townsburg, 4, 1, Direction.WEST);
+        new Wall(townsburg, 1, 4, Direction.NORTH);
+        new Wall(townsburg, 1, 4, Direction.SOUTH);
+        new Wall(townsburg, 1, 4, Direction.EAST);
+        new Wall(townsburg, 1, 4, Direction.WEST);
+        new Wall(townsburg, 4, 4, Direction.NORTH);
+        new Wall(townsburg, 4, 4, Direction.SOUTH);
+        new Wall(townsburg, 4, 4, Direction.EAST);
+        new Wall(townsburg, 4, 4, Direction.WEST);
+        new Wall(townsburg, 2, 2, Direction.NORTH);
+        new Wall(townsburg, 2, 2, Direction.WEST);
+        new Wall(townsburg, 3, 2, Direction.SOUTH);
+        new Wall(townsburg, 3, 2, Direction.WEST);
+        new Wall(townsburg, 3, 3, Direction.SOUTH);
+        new Wall(townsburg, 3, 3, Direction.EAST);
+        new Wall(townsburg, 2, 3, Direction.NORTH);
+        new Wall(townsburg, 2, 3, Direction.EAST);
 
-     while(andy.getAvenue()==2)  {
-     
-       andy.move();
-       andy.turnLeft();
-       andy.move(1);
-       andy.turnRight();
-       andy.move(2);
-       andy.turnRight();
-       andy.move(2);
-       andy.turnRight();
-       
-     
-}
 
-}
+
+
+
+        // while there is nothing in front of andy...
+        while (andy.frontIsClear()) {
+
+            /**
+             * if the street number that andy is on is less than or equal to 4
+             * andy will patrol the castle
+             */
+            if (andy.getStreet() <= 4) {
+
+                andy.move();
+                andy.turnLeft();
+                andy.move(1);
+                andy.turnRight();
+                andy.move(2);
+                andy.turnRight();
+                andy.move(2);
+                andy.turnRight();
+                andy.move();
+                andy.turnLeft();
+
+            }
+
+        }
+    }
 }
