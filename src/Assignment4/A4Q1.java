aea329aaccda5e812d439340d263e523581325a5
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-03-01 10:25:52 -0500

    A4Q1 Completed.

---
+++ src/Assignment4/A4Q1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author watsa9604
+ */
+public class A4Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       // use a scanner to read info
+        Scanner input = new Scanner(System.in);
+        
+         System.out.println("Please enter your name.");
+         
+        //make a string
+        String name = input.nextLine();
+        
+        //say hello
+        System.out.println("Hello " + name + " How are you today? " );
+    }
+}
