d4920855665eea1d7287a94b560504c3e2afc908
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-03-19 10:22:56 -0400

    A4Q10 Completed

--- src/Assignment2/A2Q1.java
+++ src/Assignment2/A2Q1.java
@@ -20,48 +20,49 @@ public class A2Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     // Create a city for the Robot      
-  City townsburg = new City();
-  
-  // Put a Robot in Townsburg
-  RobotSE andy = new RobotSE(townsburg, 1, 1, Direction.EAST);
-  
-  andy.setLabel("A");
-  
-  //Set the color of the Robot
-  andy.setColor(Color.CYAN);
-  
-  //create the 10 things
-  new Thing (townsburg,1,2);
-  new Thing (townsburg,1,3);
-  new Thing (townsburg,1,4);
-  new Thing (townsburg,1,5);
-  new Thing (townsburg,1,6);
-  new Thing (townsburg,1,7);
-  new Thing (townsburg,1,8);
-  new Thing (townsburg,1,9);
-  new Thing (townsburg,1,10);
-  new Thing (townsburg,1,11);
-  
-  //make andy move to the things
-  andy.move();
-  //while andy can pick a thing the next if statments will activate
-  while(andy.canPickThing()){
-  
-  //is there something to pick up? If yes then andy will pick the thing then move
-  if(andy.canPickThing()){
-  andy.pickThing();
-  andy.move();
-  //if the number of things in andy's backpack = 7 he will move 3 spots
-  if(andy.countThingsInBackpack()==7){
-  andy.move(3);
- 
-/** because andy moved three spots he landed on a space with no things, thus he 
-*stopped moving
-*/
-  } 
-  }
-  
-}
-}
+        // Create a city for the Robot      
+        City townsburg = new City();
+
+        // Put a Robot in Townsburg
+        RobotSE andy = new RobotSE(townsburg, 1, 1, Direction.EAST);
+
+        andy.setLabel("A");
+
+        //Set the color of the Robot
+        andy.setColor(Color.CYAN);
+
+        //create the 10 things
+        new Thing(townsburg, 1, 2);
+        new Thing(townsburg, 1, 3);
+        new Thing(townsburg, 1, 4);
+        new Thing(townsburg, 1, 5);
+        new Thing(townsburg, 1, 6);
+        new Thing(townsburg, 1, 7);
+        new Thing(townsburg, 1, 8);
+        new Thing(townsburg, 1, 9);
+        new Thing(townsburg, 1, 10);
+        new Thing(townsburg, 1, 11);
+
+        //make andy move to the things
+        andy.move();
+        //while andy can pick a thing the next if statments will activate
+        while (andy.canPickThing()) {
+
+            //is there something to pick up? If yes then andy will pick the thing then move
+            if (andy.canPickThing()) {
+                andy.pickThing();
+                andy.move();
+                //if the number of things in andy's backpack = 7 he will move 3 spots
+                if (andy.countThingsInBackpack() == 7) {
+                    andy.move(3);
+
+                    /**
+                     * because andy moved three spots he landed on a space with
+                     * no things, thus he stopped moving
+                     */
+                }
+            }
+
+        }
+    }
 }
