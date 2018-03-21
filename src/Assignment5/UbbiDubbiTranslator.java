25b26ba5789f1bc8bc278831451311d9620cf0c8
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-03-21 11:14:02 -0400

    Ubbi Dubbi not even close.

--- src/Assignment5/UbbiDubbiTranslator.java
+++ src/Assignment5/UbbiDubbiTranslator.java
@@ -27,24 +27,36 @@ public class UbbiDubbiTranslator {
             
             //walk down the word looking for the vowel
                 int length = startWord.length();
+                
+                String complete = "";
+                
                 //use a loop to go through the characters
                 for (int i = 0; i < length; i++) {
-                    
+                
                     //is the character at position "i" a vowel?
                     if (startWord.charAt(i) == 'a'
                             || startWord.charAt(i) == 'e'
                             || startWord.charAt(i) == 'i'
                             || startWord.charAt(i) == 'o'
                             || startWord.charAt(i) == 'u') {
+                        
+                  
+                        
+                        
+                        
+                        
                         //i is the position of the vowel
-                        String start = startWord.substring(0, i);
+                        //String start = startWord.substring(0, i);
                         //break the word apart at the vowel
-                        String end = startWord.substring(i);
-
-                        //compute translated word
-                        String reverse = start + "ub" + end;
-                        System.out.println(startWord + " in Ubbi Dubbi is " + reverse);
-
+                        //String end = startWord.substring(i);                        
+                                              
+                             //System.out.println(startWord + " in Ubbi Dubbi is " + complete3);
+                            
+                        
+                      
+           
+                        
+                       
                         
             
           
@@ -53,3 +65,4 @@ public class UbbiDubbiTranslator {
 }
 }
 }
+
