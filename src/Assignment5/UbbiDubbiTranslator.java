f70e2ed2c85156d521fe91cf994d35ce20b58cb8
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-03-22 11:03:17 -0400

    Ubbi Dubbi SUPER CLOSE!!

--- src/Assignment5/UbbiDubbiTranslator.java
+++ src/Assignment5/UbbiDubbiTranslator.java
@@ -16,53 +16,49 @@ public class UbbiDubbiTranslator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-           Scanner input = new Scanner(System.in);
+        
+        Scanner input = new Scanner(System.in);
         //repeat the translator
         while (true) {
+            
             //get the word to translate
             System.out.println("Enter a word to translate");
             String startWord = input.nextLine();
+            
             //sanitize the input
             startWord = startWord.toLowerCase();
-            
-            //walk down the word looking for the vowel
-                int length = startWord.length();
-                
-                String complete = "";
-                
-                //use a loop to go through the characters
-                for (int i = 0; i < length; i++) {
-                
-                    //is the character at position "i" a vowel?
-                    if (startWord.charAt(i) == 'a'
-                            || startWord.charAt(i) == 'e'
-                            || startWord.charAt(i) == 'i'
-                            || startWord.charAt(i) == 'o'
-                            || startWord.charAt(i) == 'u') {
-                        
-                  
-                        
-                        
-                        
-                        
-                        //i is the position of the vowel
-                        //String start = startWord.substring(0, i);
-                        //break the word apart at the vowel
-                        //String end = startWord.substring(i);                        
-                                              
-                             //System.out.println(startWord + " in Ubbi Dubbi is " + complete3);
-                            
-                        
-                      
-           
-                        
-                       
-                        
-            
-          
-}
-}
-}
-}
-}
 
+            //walk down the word looking for the vowel
+            int length = startWord.length();
+            
+            //create an empty string
+            String complete = "";
+
+            //use a loop to go through the characters
+            for (int i = 0; i < length; i++) {
+
+                //is the character at position "i" a vowel?
+                if (startWord.charAt(i) == 'a'
+                        || startWord.charAt(i) == 'e'
+                        || startWord.charAt(i) == 'i'
+                        || startWord.charAt(i) == 'o'
+                        || startWord.charAt(i) == 'u') {
+                    
+                    //"ub" will be placed in front of any vowel
+                    complete += "ub";
+                }
+                //adds the word that the user put in, to the empty string
+                complete += startWord.charAt(i);
+                /*
+                 * because the original word was put into the empty string, all 
+                 * of the vowels had "ub" added in front of them.
+                 */
+                
+                
+            }           
+            //tell the user their translated word
+            System.out.println(startWord + " in Ubbi Dubbi is " + complete);
+
+        }
+    }
+}
