4f43b541734ab7701aa49262443d5a542595ee4b
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-03-29 11:06:59 -0400

    Ubbi Dubbi still SUPER CLOSE!! BUT NOT REALLY CLOSE AT ALL!!

--- src/Assignment4/A4Q9.java
+++ src/Assignment4/A4Q9.java
@@ -154,65 +154,64 @@ public class A4Q9 extends javax.swing.JFrame {
 
     private void Number1TextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Number1TextActionPerformed
         String num1 = Number1Text.getText();
-        
+
     }//GEN-LAST:event_Number1TextActionPerformed
 
     private void Number2TextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Number2TextActionPerformed
         String num2 = Number2Text.getText();
-        
+
     }//GEN-LAST:event_Number2TextActionPerformed
 
     private void ResultTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ResultTextActionPerformed
         // TODO add your handling code here:
-        
     }//GEN-LAST:event_ResultTextActionPerformed
 
     private void AddingActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AddingActionPerformed
-       String num2 = Number2Text.getText();
-       String num1 = Number1Text.getText();
-       int num2a = Integer.parseInt(num2);
-       int num1a = Integer.parseInt(num1);
-       
-       int added = num2a + num1a;
-       
-       ResultText.setText("" +added);
-        
+        String num2 = Number2Text.getText();
+        String num1 = Number1Text.getText();
+        int num2a = Integer.parseInt(num2);
+        int num1a = Integer.parseInt(num1);
+
+        int added = num2a + num1a;
+
+        ResultText.setText("" + added);
+
     }//GEN-LAST:event_AddingActionPerformed
 
     private void SubtractingActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SubtractingActionPerformed
-       String num2 = Number2Text.getText();
-       String num1 = Number1Text.getText();
-       int num2a = Integer.parseInt(num2);
-       int num1a = Integer.parseInt(num1);
-       
-       int subbed = num1a - num2a;
-       
-       ResultText.setText("" +subbed);
-       
+        String num2 = Number2Text.getText();
+        String num1 = Number1Text.getText();
+        int num2a = Integer.parseInt(num2);
+        int num1a = Integer.parseInt(num1);
+
+        int subbed = num1a - num2a;
+
+        ResultText.setText("" + subbed);
+
     }//GEN-LAST:event_SubtractingActionPerformed
 
     private void MultiplyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MultiplyActionPerformed
-       String num2 = Number2Text.getText();
-       String num1 = Number1Text.getText();
-       int num2a = Integer.parseInt(num2);
-       int num1a = Integer.parseInt(num1);
-       
-       int multi = num2a * num1a;
-       
-       ResultText.setText("" +multi);
-        
+        String num2 = Number2Text.getText();
+        String num1 = Number1Text.getText();
+        int num2a = Integer.parseInt(num2);
+        int num1a = Integer.parseInt(num1);
+
+        int multi = num2a * num1a;
+
+        ResultText.setText("" + multi);
+
     }//GEN-LAST:event_MultiplyActionPerformed
 
     private void DivideActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DivideActionPerformed
-      String num2 = Number2Text.getText();
-       String num1 = Number1Text.getText();
-       int num2a = Integer.parseInt(num2);
-       int num1a = Integer.parseInt(num1);
-       
-       int divide = num1a / num2a;
-       
-       ResultText.setText("" +divide);
-        
+        String num2 = Number2Text.getText();
+        String num1 = Number1Text.getText();
+        int num2a = Integer.parseInt(num2);
+        int num1a = Integer.parseInt(num1);
+
+        int divide = num1a / num2a;
+
+        ResultText.setText("" + divide);
+
     }//GEN-LAST:event_DivideActionPerformed
 
     /**
