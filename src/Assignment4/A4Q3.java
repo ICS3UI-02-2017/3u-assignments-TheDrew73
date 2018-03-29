06528d1b977e5ff50e4fa2841b2b2bacde8d1377
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-03-29 11:05:57 -0400

    Ubbi Dubbi still SUPER CLOSE!! BUT NOT REALLY CLOSE AT ALL!!

--- src/Assignment4/A4Q3.java
+++ src/Assignment4/A4Q3.java
@@ -16,21 +16,21 @@ public class A4Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         // use a scanner to read info
-         Scanner input = new Scanner(System.in);
-        
-         //ask the user for their 4 numbers
+        Scanner input = new Scanner(System.in);
+
+        //ask the user for their 4 numbers
         System.out.println("Please enter in 4 numbers on different lines");
-        
+
         //create 4 variables for the numbers
         double number1 = input.nextDouble();
         double number2 = input.nextDouble();
         double number3 = input.nextDouble();
         double number4 = input.nextDouble();
-        
+
         //tell the user their numbers
-        System.out.println("Your numbers were "+number1+", "+number2+", "+number3+", "+number4);
-        
+        System.out.println("Your numbers were " + number1 + ", " + number2 + ", " + number3 + ", " + number4);
+
     }
 }
