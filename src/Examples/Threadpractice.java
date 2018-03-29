03dc44a26e7709167d871994b3efa9dd77f9e376
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-03-29 11:09:09 -0400

    Ubbi Dubbi still SUPER CLOSE!! BUT NOT REALLY CLOSE AT ALL!!

--- src/Examples/Threadpractice.java
+++ src/Examples/Threadpractice.java
@@ -21,47 +21,47 @@ public class Threadpractice {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-         //create city for Robot
-    City townsburg = new City();
-    
-    //create new Robot
-    final RobotSE andy = new RobotSE (townsburg, 0, 0, Direction.SOUTH);
-    
-    //create second Robot
-    RobotSE bob = new RobotSE (townsburg, 0, 1, Direction.SOUTH);
-    
-    //Label Robot
-    andy.setLabel("A");   
-    
-    //Label Second Robot
-    bob.setLabel ("B");
-    
-    //make bob a different colour
-    bob.setColor (Color.CYAN);
-    
-     //create wall
-    new Wall (townsburg, 0, 1, Direction.WEST);
-    new Wall (townsburg, 1, 1, Direction.WEST);
-    new Wall (townsburg, 1, 1, Direction.SOUTH);
-        
-        new Thread(){
-            public void run(){
+
+        //create city for Robot
+        City townsburg = new City();
+
+        //create new Robot
+        final RobotSE andy = new RobotSE(townsburg, 0, 0, Direction.SOUTH);
+
+        //create second Robot
+        RobotSE bob = new RobotSE(townsburg, 0, 1, Direction.SOUTH);
+
+        //Label Robot
+        andy.setLabel("A");
+
+        //Label Second Robot
+        bob.setLabel("B");
+
+        //make bob a different colour
+        bob.setColor(Color.CYAN);
+
+        //create wall
+        new Wall(townsburg, 0, 1, Direction.WEST);
+        new Wall(townsburg, 1, 1, Direction.WEST);
+        new Wall(townsburg, 1, 1, Direction.SOUTH);
+
+        new Thread() {
+            public void run() {
                 // things to do
-    andy.move(); 
-    andy.move();
-    andy.turnLeft();
-    andy.move();
+                andy.move();
+                andy.move();
+                andy.turnLeft();
+                andy.move();
             }
         }.start();
-        
-    bob.move();
-    bob.turnLeft();
-    bob.move();
-    bob.turnRight();
-    bob.move();
-    bob.turnRight();
-    bob.move();
-        
+
+        bob.move();
+        bob.turnLeft();
+        bob.move();
+        bob.turnRight();
+        bob.move();
+        bob.turnRight();
+        bob.move();
+
     }
 }
