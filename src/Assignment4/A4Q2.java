7cddee8dbe6168f2bdb92c577d27927497eb6eef
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-03-29 11:05:44 -0400

    Ubbi Dubbi still SUPER CLOSE!! BUT NOT REALLY CLOSE AT ALL!!

--- src/Assignment4/A4Q2.java
+++ src/Assignment4/A4Q2.java
@@ -16,32 +16,32 @@ public class A4Q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
-        
-      // use a scanner to read info
-         Scanner input = new Scanner(System.in);
-        
-         //opening line to the user
-         System.out.println("Please enter the measurement in inches you wish to convert.");
-         
-         //setting the variables
-         double conversion = input.nextDouble();
-         double number = conversion * 2.54;
-         
-         //if the user tries to get smart...
-          if(conversion <= 0) {
-               System.out.println("Try again...");
-           conversion = input.nextInt();
-           number = conversion * 2.54;
-           
-        } 
-         //final statement that will give the user the conversion info
-         System.out.println( conversion + " inches is the same as " + number + " cm ");
-         
-          
-        
-        
-    
-          
-}
+
+
+        // use a scanner to read info
+        Scanner input = new Scanner(System.in);
+
+        //opening line to the user
+        System.out.println("Please enter the measurement in inches you wish to convert.");
+
+        //setting the variables
+        double conversion = input.nextDouble();
+        double number = conversion * 2.54;
+
+        //if the user tries to get smart...
+        if (conversion <= 0) {
+            System.out.println("Try again...");
+            conversion = input.nextInt();
+            number = conversion * 2.54;
+
+        }
+        //final statement that will give the user the conversion info
+        System.out.println(conversion + " inches is the same as " + number + " cm ");
+
+
+
+
+
+
+    }
 }
