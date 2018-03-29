379b9166c17104073af203681b2541dfe5963974
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-03-29 11:08:02 -0400

    Ubbi Dubbi still SUPER CLOSE!! BUT NOT REALLY CLOSE AT ALL!!

--- src/Assignment3/A3Q4.java
+++ src/Assignment3/A3Q4.java
@@ -20,10 +20,10 @@ public class A3Q4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         int numberOfMoves = 0;
         int repeat = 0;
-        
+
         //Create a city for the Robot      
         City townsburg = new City();
 
@@ -34,67 +34,67 @@ public class A3Q4 {
         andy.setColor(Color.CYAN);
 
         andy.setLabel("A");
-        
+
         //create the first square
-        new Wall (townsburg, 1,1, Direction.WEST);
-        new Wall (townsburg, 1,1, Direction.NORTH);
-        new Wall (townsburg, 1,2, Direction.EAST);
-        new Wall (townsburg, 1,2, Direction.NORTH);
-        new Wall (townsburg, 2,1, Direction.SOUTH);
-        new Wall (townsburg, 2,1, Direction.WEST);
-        new Wall (townsburg, 2,2, Direction.EAST);
-        new Wall (townsburg, 2,2, Direction.SOUTH);
-        
+        new Wall(townsburg, 1, 1, Direction.WEST);
+        new Wall(townsburg, 1, 1, Direction.NORTH);
+        new Wall(townsburg, 1, 2, Direction.EAST);
+        new Wall(townsburg, 1, 2, Direction.NORTH);
+        new Wall(townsburg, 2, 1, Direction.SOUTH);
+        new Wall(townsburg, 2, 1, Direction.WEST);
+        new Wall(townsburg, 2, 2, Direction.EAST);
+        new Wall(townsburg, 2, 2, Direction.SOUTH);
+
         //create the Second square
-        new Wall (townsburg, 1,4, Direction.WEST);
-        new Wall (townsburg, 1,4, Direction.NORTH);
-        new Wall (townsburg, 1,5, Direction.EAST);
-        new Wall (townsburg, 1,5, Direction.NORTH);
-        new Wall (townsburg, 2,4, Direction.WEST);
-        new Wall (townsburg, 2,4, Direction.SOUTH);
-        new Wall (townsburg, 2,5, Direction.EAST);
-        new Wall (townsburg, 2,5, Direction.SOUTH);
-        
+        new Wall(townsburg, 1, 4, Direction.WEST);
+        new Wall(townsburg, 1, 4, Direction.NORTH);
+        new Wall(townsburg, 1, 5, Direction.EAST);
+        new Wall(townsburg, 1, 5, Direction.NORTH);
+        new Wall(townsburg, 2, 4, Direction.WEST);
+        new Wall(townsburg, 2, 4, Direction.SOUTH);
+        new Wall(townsburg, 2, 5, Direction.EAST);
+        new Wall(townsburg, 2, 5, Direction.SOUTH);
+
         //create the third square
-        new Wall (townsburg, 4,1, Direction.WEST);
-        new Wall (townsburg, 4,1, Direction.NORTH);
-        new Wall (townsburg, 5,1, Direction.WEST);
-        new Wall (townsburg, 5,1, Direction.SOUTH);
-        new Wall (townsburg, 4,2, Direction.EAST);
-        new Wall (townsburg, 4,2, Direction.NORTH);
-        new Wall (townsburg, 5,2, Direction.EAST);
-        new Wall (townsburg, 5,2, Direction.SOUTH);
-        
+        new Wall(townsburg, 4, 1, Direction.WEST);
+        new Wall(townsburg, 4, 1, Direction.NORTH);
+        new Wall(townsburg, 5, 1, Direction.WEST);
+        new Wall(townsburg, 5, 1, Direction.SOUTH);
+        new Wall(townsburg, 4, 2, Direction.EAST);
+        new Wall(townsburg, 4, 2, Direction.NORTH);
+        new Wall(townsburg, 5, 2, Direction.EAST);
+        new Wall(townsburg, 5, 2, Direction.SOUTH);
+
         //create the fourth square
-        new Wall (townsburg, 4,4, Direction.WEST);
-        new Wall (townsburg, 4,4, Direction.NORTH);
-        new Wall (townsburg, 4,5, Direction.EAST);
-        new Wall (townsburg, 4,5, Direction.NORTH);
-        new Wall (townsburg, 5,4, Direction.WEST);
-        new Wall (townsburg, 5,4, Direction.SOUTH);
-        new Wall (townsburg, 5,5, Direction.EAST);
-        new Wall (townsburg, 5,5, Direction.SOUTH);
-        
+        new Wall(townsburg, 4, 4, Direction.WEST);
+        new Wall(townsburg, 4, 4, Direction.NORTH);
+        new Wall(townsburg, 4, 5, Direction.EAST);
+        new Wall(townsburg, 4, 5, Direction.NORTH);
+        new Wall(townsburg, 5, 4, Direction.WEST);
+        new Wall(townsburg, 5, 4, Direction.SOUTH);
+        new Wall(townsburg, 5, 5, Direction.EAST);
+        new Wall(townsburg, 5, 5, Direction.SOUTH);
+
         //andy will repeat the entire sequence 4 times
         repeat = 0;
-        while(repeat < 4){
-        repeat = repeat + 1;
-        
-        //and will round the corner of a square 3 times
-        numberOfMoves = 0;
-        while(numberOfMoves < 3){
-        andy.move(3);
-        andy.turnLeft();
-        numberOfMoves =  numberOfMoves + 1;
-        
-        
-}
-        // andy will move forward 3 spaces
-        for (int count = 0; count < 3; count++) {
-            andy.move();
+        while (repeat < 4) {
+            repeat = repeat + 1;
+
+            //and will round the corner of a square 3 times
+            numberOfMoves = 0;
+            while (numberOfMoves < 3) {
+                andy.move(3);
+                andy.turnLeft();
+                numberOfMoves = numberOfMoves + 1;
+
+
+            }
+            // andy will move forward 3 spaces
+            for (int count = 0; count < 3; count++) {
+                andy.move();
+            }
+        }
+
+
     }
-}
-       
-         
-}
 }
