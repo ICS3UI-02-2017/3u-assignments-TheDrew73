ce71f09f85f4a6296e39e30f744dd53285ce0d39
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-04-06 11:18:46 -0400

    A6Q1 just started.

--- src/Assignment6/A6Q1.java
+++ src/Assignment6/A6Q1.java
@@ -16,32 +16,39 @@ public class A6Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
-        
-        double[] percents = new double[3];
-        
+
+        double[] percents = new double[6];
+
+        double students = 0;
+
         System.out.println("How many students are in the class?");
-        String students = input.nextLine();
+
+        for (int i = 0; i == students;) {
+            students = input.nextDouble();
+        }
 
         System.out.println("Enter the marks");
-        
+
         for (int i = 0; i < percents.length; i++) {
             percents[i] = input.nextDouble();
-            
+
         }
+
         //adding all of the expenses
         double sum = 0;
+        double average = 0;
 
         for (int i = 0; i < percents.length; i++) {
 
             //get the expense
-            double average = percents[i];
+            average = average + percents[i];
 
-            sum = sum + average % percents[i];
-            
-    }
+        }
         
+        sum = average / students;
+
         System.out.println("The class average is " + sum);
-}
+    }
 }
