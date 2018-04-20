6a6fb47e5f739746a047371d1e4f3c307629ce89
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-04-20 11:47:13 -0400

    A6Q7 just started.

---
+++ src/Assignment6/A6Q7.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author watsa9604
+ */
+public class A6Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //creates a scanner
+        Scanner input = new Scanner(System.in);
+        
+        int count = 1;
+        
+        int number = 2;
+        
+        boolean primeNums = true;
+        
+        
+        
+        //int[] prime = new int [2, 1000];
+        
+        
+    }
+}
