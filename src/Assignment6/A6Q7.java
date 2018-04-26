0008ef077852068655abaab0bf2acb3167d77014
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-04-26 11:18:42 -0400

    A6Q7 not even the slightest bit close. And im pretty sure I'm slowly getting further and further from the answer. And also maybe going slightly insane. :)

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -19,32 +19,18 @@ public class A6Q7 {
         //creates a scanner
         Scanner input = new Scanner(System.in);
 
+        int j = 0;
+        
         int i = 2;
 
-        int[] prime = new int[999];
+        int[] prime = new int [999];
 
         boolean[] flag = new boolean[999];
 
+        
+        
         for (i = 2; i <= flag.length; i++) {
-            flag[i] = true;
-
+            
         }
-
-//        if (i + 2 > i){
-//            i = prime[i];
-//            flag[i] = true;
-//        } else {
-//            flag[i] = false;
-//        }
-//        
-//        
-//      System.out.println(flag[i]);
-
-
-
-
-//        boolean primeNums = true;
-
-
     }
 }
