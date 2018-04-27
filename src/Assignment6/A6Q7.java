1a3213e4acfdd3c16012b993f57fee8167e6eb97
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-04-27 11:04:32 -0400

    A6Q7 completed.

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -19,18 +19,42 @@ public class A6Q7 {
         //creates a scanner
         Scanner input = new Scanner(System.in);
 
-        int j = 0;
-        
+        //setting an integer equal to 0
+        int primes = 0;
+
+        //setting the first prime which is two
         int i = 2;
 
-        int[] prime = new int [999];
-
+        //createing a boolean array that contains numbers from 2 - 999
         boolean[] flag = new boolean[999];
 
+        //uses the boolean array to mark every number in the array 
+        for (i = 2; i < flag.length; i++) {
+            flag[i] = true;
+        }
+        System.out.println("The prime numbers from 2 - 1000 are; ");
         
-        
-        for (i = 2; i <= flag.length; i++) {
-            
+        //looks though the array
+        for (i = 2; i < flag.length; i++) {
+
+            //sets primes equal to whatever "i" is
+            primes = i;
+
+            //counts up by an increment of whatever primes is
+            int spot = 2 * primes;
+
+            //if a number in the array is marked as true output them to the user
+            if (flag[i] == true) {
+                System.out.println(primes);
+
+                //while marks every prime number in the boolean array as false
+                while (spot < 999) {
+
+                    flag[spot] = false;
+
+                    spot = spot + primes;
+                }
+            }
         }
     }
 }
