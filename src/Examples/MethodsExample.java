6098b42028ac97fde63e60752c76abe055f3a339
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-04-27 11:13:05 -0400

    Methods example. but I haven't found one yet.

---
+++ src/Examples/MethodsExample.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author watsa9604
+ */
+public class MethodsExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+}
+
+
+        	
+
