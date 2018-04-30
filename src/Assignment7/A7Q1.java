45a7789d722baf8da675da7897b9b4fab1297eae
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-04-30 10:53:31 -0400

    A7Q1 completed.

--- src/Assignment7/A7Q1.java
+++ src/Assignment7/A7Q1.java
@@ -4,16 +4,34 @@
  */
 package Assignment7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author watsa9604
  */
 public class A7Q1 {
 
+    public static void circleArea() {
+
+        //creates a scanner
+        Scanner input = new Scanner(System.in);
+        
+        //asks the user for the radius
+        System.out.println("Please enter the radius.");
+        
+        //calculates the area of the circle and outputs it to the user
+        double radius = input.nextInt();
+        double area = 3.14159*radius*radius;
+        Math.ceil(area);
+        System.out.println("The area of the circle is " + area);
+    }
+            
+            
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        circleArea();
     }
 }
