62aeb151ac2ea87daabcf02328136a34f55b5a03
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-05-01 10:45:31 -0400

    A7Q3 completed.

--- src/Assignment7/A7Q3.java
+++ src/Assignment7/A7Q3.java
@@ -10,10 +10,24 @@ package Assignment7;
  */
 public class A7Q3 {
 
+    public static void factors(int num) {
+
+        //tells the user the factors of whatever number they entered
+        System.out.println("the factors of " + num + " are: ");
+
+        //calculates all of the fators for the number that the user wants
+        for (int i = 1; i < 9999; i++) {
+            if (num % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        //is the number that the user wants
+        factors(10);
     }
 }
