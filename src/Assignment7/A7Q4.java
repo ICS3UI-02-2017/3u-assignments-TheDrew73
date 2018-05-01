e744cff7f79aee9b55034384097aa24d2f799b7e
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-05-01 11:12:21 -0400

    A7Q4 Just started.

---
+++ src/Assignment7/A7Q4.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author watsa9604
+ */
+public class A7Q4 {
+
+    public static double compoundInterest(double initial) {
+        
+        
+        
+        
+        return initial;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         //creates a scanner
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please enter your initial amount.");
+         double last = input.nextDouble();
+         
+    }
+}
