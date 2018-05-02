530b4ff9396047556ca71b1d4446190a291dcf64
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-05-02 11:08:27 -0400

    A7Q6 completed.

---
+++ src/Assignment7/A7Q6.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment7;
+
+/**
+ *
+ * @author watsa9604
+ */
+public class A7Q6 {
+
+    public static double lastDigit(double lastDig) {
+        
+
+        
+        
+        return lastDig;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        lastDigit(3573);
+        
+        
+    }
+}
