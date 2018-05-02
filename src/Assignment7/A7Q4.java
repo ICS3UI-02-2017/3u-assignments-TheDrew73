72974ef275bcc2ca6c964350afc70faed70f105f
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-05-02 10:32:04 -0400

    A7Q4 maybe completed????

--- src/Assignment7/A7Q4.java
+++ src/Assignment7/A7Q4.java
@@ -14,9 +14,15 @@ public class A7Q4 {
 
     public static double compoundInterest(double initialValue) {
 
-        
-        
         return initialValue;
+    }
+    public static double compoundInterest2(double rateValue) {
+        
+       return rateValue;
+    }
+    public static double compoundInterest3(double years) {
+        
+        return years;
     }
 
     /**
@@ -27,7 +33,16 @@ public class A7Q4 {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Please enter your initial amount.");
-        double lastValue = input.nextDouble();
+        double initialValue = input.nextDouble();
 
-    }
+        System.out.println("Please enter the current interest rate.");
+        double rateValue = input.nextDouble();
+        
+        System.out.println("In years, how long do you plan on using the account for?");
+        double years = input.nextDouble();
+        
+        double balance = initialValue * rateValue + 1; Math.pow(rateValue + 1, years);
+        System.out.println("Your total balance after " + years + " years is " + balance);
+        
+    }   
 }
