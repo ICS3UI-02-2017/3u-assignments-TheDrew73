0dafb334614162454c4d98ef8c4f0b421e91361a
watsa9604 <watsa9604@HRH-IC0027330.wrdsb.ca>
2018-05-03 11:14:09 -0400

    A7Q8 just started.

---
+++ src/Assignment7/A7Q8.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment7;
+
+/**
+ *
+ * @author watsa9604
+ */
+public class A7Q8 {
+
+    public static int allDigitsOdd(int allOdd) {
+
+
+
+
+        return allOdd;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        allDigitsOdd(9056342);
+    }
+}
